21bfdfac85211f66e9d231fa1fa4cb1a2c3b2f38
David Inyang <darexunlimited@gmail.com>
2021-08-17 23:01:33 +0100

    Create head.php

---
+++ resume/inc/head.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+		<title><?php echo $lng['name'].' :: '.$lng['site-name'] ;?></title>
+		<meta name="description" content="<?php echo $lng['meta-desc'] ;?> ">
+		<link href="./css/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+		<link rel="stylesheet" href="./css/bootstrap.min.css">
+		<link rel="stylesheet" href="./css/animate.css">
+		<link rel="stylesheet" href="./css/csse8f1.css?family=Nunito:200,300,400,600,700,800,900">
+		<link rel="stylesheet" href="./css/main.css">
+		<link href="./css/pnotify.css" rel="stylesheet">
+		<link href="./css/pnotify.buttons.css" rel="stylesheet">
+		<link href="./css/pnotify.nonblock.css" rel="stylesheet">
+   </head>
+	<body>
+
+
+<div id="page_content">
