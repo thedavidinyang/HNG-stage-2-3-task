bca4b3a21d9d18da6c07db9ef171df40afbdbd19
David Inyang <darexunlimited@gmail.com>
2021-08-17 23:00:38 +0100

    Create 404.php

---
+++ resume/loaders/404.php
@@ -0,0 +1,11 @@
+<?php
+
+# HNGi8 STAGE 2 => 3 PROJECT
+# Author: David Inyang
+# Email: davidinyang01@gmail.com
+# phone: +2348182412241
+# Language: English
+
+die('<h1>A 404 error occured, Please refresh page</h1>');
+
+?>
