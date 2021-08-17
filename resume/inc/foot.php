9b26f998f0e752317adfe1d4908fc095c493cacd
David Inyang <darexunlimited@gmail.com>
2021-08-17 23:01:39 +0100

    Create foot.php

---
+++ resume/inc/foot.php
@@ -0,0 +1,10 @@
+</div>
+<script src="./js/jquery-3.3.1.min.js"></script>
+<script src="./js/bootstrap.min.js"></script>
+<script src="./js/wow.min.js"></script>
+<script>  new WOW().init(); </script>
+<script src="./js/pnotify.js"></script>
+<script src="./js/pnotify.buttons.js"></script>
+<script src="./js/pnotify.nonblock.js"></script>
+</body>
+</html>
